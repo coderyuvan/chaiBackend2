f6ebda2a7f7eb2491b58ab3d077c2b8c25e09aea
unknown <1095yuvangrover@gmail.com>
2024-07-02 20:52:05 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 4000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+app.get('/twitter',(req,res)=>{
+    res.send('hiteshdotcom')
+})
+app.get('/goggle',(req,res)=>{
+    res.send('YuvanSir')
+})
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening on port ${port}`)
+})
